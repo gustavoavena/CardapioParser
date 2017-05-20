b89c6144bd0dd38088cc5315bf8d7f3378a008f4
Gustavo Avena <ggavena@gmail.com>
2017-05-20 16:10:17 -0300

    consigo pegar o cardapio de um dia e retornar um JSON com seus valores

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pprint
 
 
 # file = open("cardapio.html", "r")
@@ -27,6 +28,9 @@ def main():
         preenche_refeicao(cardapio, refeicoes[i], m)
 
 
+    # pprint.pprint(cardapio)
+
+
 
 # Exemplo:
 # ['ARROZ E FEIJÃO']  = ['NavigableString']
@@ -39,72 +43,39 @@ def main():
 # [<br/>, <strong>Observações:</strong>, ' ', <br/>, ' O CARDÁPIO CONTÉM GLÚTEN NO PÃO E NO CREME DE ESPINAFRE. CONTÉM LACTOSE NO CREME DE ESPINAFRE. NÃO CONTÉM OVOS.']  = ['Tag', 'Tag', 'NavigableString', 'Tag', 'NavigableString']
 
 
-def pega_salada_sobremsa_suco(items):
+def pega_salada_sobremesa_suco(items):
     alimentos = ["salada", "suco", "sobremesa"]
     cardapio = {}
 
-    print(cardapio)
-    print(items)
-    print("\n")
     for i, alim in enumerate(alimentos):
         tag = alimentos[i].upper() + ":"
-
         valor = [s.replace(tag, "") for s in items if tag in s][0]
-
         cardapio[alimentos[i]] = valor
         items = [s for s in items if tag not in s]
 
-    print(cardapio)
-    print(items)
+
+    return cardapio, items
 
 
 def preenche_refeicao(cardapio_do_dia, refeicao, soup):
     cardapio = {}
 
-    # items = soup.find_all('td')
-    # print("items.size = ", len(items))
     items = [s for s in soup.get_text().split("\n") if s]
-    # print(items)
-    # for item in items:
-    #     # help(item)
-    #     print([i for i in item.children], " = ", end="")
-    #     print([type(i).__name__ for i in item.children])
+    print(items)
+
+    cardapio, items = pega_salada_sobremesa_suco(items)
 
     # pega tipo de arroz:
     cardapio["arroz_feijao"] = items.pop(0)
-    # print(items)
-
-    # salada
-    salada = [s.replace("SALADA:", "") for s in items if "SALADA:" in s][0]
-    # print(salada)
-    cardapio["salada"] = salada
-    items = [s for s in items if "SALADA:" not in s]
-
-    # sobremesa
-    sobremesa = [s.replace("SOBREMESA:", "") for s in items if "SOBREMESA:" in s][0]
-    # print(sobremesa)
-    cardapio["sobremesa"] = sobremesa
-
-    items = [s for s in items if "SOBREMESA:" not in s]
-
-    # suco
-    suco = [s.replace("SUCO:", "") for s in items if "SUCO:" in s][0]
-    # print(suco)
-    cardapio["suco"] = suco
-
-    items = [s for s in items if "SUCO:" not in s]
-
 
     # observacoes
-    observacoes = items.pop().replace("Observações:  ", "")
-    # print(observacoes)
-    cardapio["observacoes"] = observacoes
+    cardapio["observacoes"] = items.pop().replace("Observações:  ", "")
 
-    cardapio["prato_principal"] = [items.pop(0).replace("PRATO PRINCIPAL:  ", ""), *items]
+    # prato principal
+    cardapio["prato_principal"] = [items.pop(0).replace("PRATO PRINCIPAL:  ", ""), *items] # o que sobrar faz parte do prato principal
 
-    print(items)
+
     print(cardapio)
-
     print("\n\n")
 
     cardapio_do_dia[refeicao] = cardapio
