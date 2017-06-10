98efe56f331614108cd69bf41ebf205e7f139f5b
Gustavo Avena <ggavena@gmail.com>
2017-06-10 15:22:13 -0300

    documentei funcoe e atualizei o README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+
+Esse app implementado em Flask é a ponte entre a fonte de informação dos cardápios do Bandeco (pode ser HTML da pagina da Prefeitura da UNICAMP ou o API fornecido por eles) e o app para iOS.
+
+Ele obtem os cardapios e instancia objetos das classes Cardapio e Refeicao. Esse objetos são serializados e enviados como JSON para o app. Dessa maneira, caso a fonte do cardapio seja alterada, o app não será afetado, contanto que o servidor retorne objetos dessas classes.
+
+O processo:
+
+- Obter dados dos cardápios.
+- Instanciar objetos da classe Cardapio.
+- Retornar esses objetos em uma lista, no formato JSON.
+
+
+Além disso, esse servidor implementa um cache e, breve, um log dos requests, para que possamos obter estatísticas sobre os usuários.
+
 Installing:
 
 You must have on your computer: python 3, pip, virtualenv.
@@ -10,4 +24,7 @@ Execute: 'source venv/bin/activate'
 Execute: 'pip install -r requirements.txt'
 Execute: 'chmod a+x run.py'
 
-To run the app: './run.py'
+To run the app: 'python run.py'
+
+
+
--- parser.py
+++ parser.py
@@ -8,19 +8,11 @@ from app import cache
 
 URL_TEMPLATE = "http://catedral.prefeitura.unicamp.br/cardapio.php?d="
 
-# TODO: use enumeration to improve code organization.
-# TODO: change the menu strings to lowercase.
 # TODO: error handling in case the menu doesn' follow the pattern, or there is no menu for that day (weekends).
-# TODO: listar strings que nao podem ir pra lowercase (e.g. PTS, RU, RA)
-
-def main():
-    cardapio = cardapio_por_data("2017-05-22")
-
-    string = json.dumps(cardapio, indent=4, ensure_ascii=False)
-
 
 
 def pega_salada_sobremesa_suco(items):
+    """ Funcao auxiliar que popula os atributos salada, sobremesa e suco do cardapio da refeicao fornecida."""
     alimentos = ["salada", "suco", "sobremesa"]
     cardapio = {}
 
@@ -36,12 +28,14 @@ def pega_salada_sobremesa_suco(items):
 
 
 def get_refeicao(tipo, soup):
-    cardapio = {}
+    """
+    Faz o parsing do cardapio de uma refeicao (e.g. almoco, jantar), dado o elemento em HTML contendo as informacoes.
 
+    :param tipo: string que representa qual refeicao é (e.g. "Almoço", "Jantar")
+    :param soup: objeto do BeatifulSoup que contem o HTML e sera utilizado para fazer o parsing.
+    :return: dict com cardapio de uma refeicao.
+    """
     items = [s for s in soup.get_text().split("\n") if s] # retira todas as tags de HTML, e armazena todos os elementos (strings nao vazias) em uma lista.
-
-    # print(items) # pelo padrao da pagina HTML, os elementos estao sempre na mesma ordem.
-
     cardapio, items = pega_salada_sobremesa_suco(items)
 
     cardapio["arroz_feijao"] = items.pop(0).capitalize()
@@ -54,13 +48,15 @@ def get_refeicao(tipo, soup):
 
     ACRONIMOS = ["pts", " ru ", " ra ", " rs "]
 
-    for sig in ACRONIMOS:
+    for sig in ACRONIMOS: # procura siglas e deixa elas em maiusculo.
         cardapio["observacoes"].replace(sig, sig.upper())
 
 
-    # o primeiro item nesse momento eh sempre o prato principal.
+    # o primeiro item nesse ponto eh sempre o prato principal.
     cardapio["prato_principal"] = items.pop(0).replace("PRATO PRINCIPAL:  ", "").capitalize()
 
+
+    # TODO: organizar isso da guarnicao e do PTS.
     # o que sobra eh a guarnicao e o pts.
     cardapio["guarnicao"] = items[0].capitalize().replace("pts", "PTS")
 
@@ -75,13 +71,19 @@ def get_refeicao(tipo, soup):
 
 
 def cardapio_por_data(data_string):
+    """
+    Dada a string de uma data, fornece o objeto da classe Cardapio correspondente ao cardapio dessa data.
+
+    :param data_string: string da data do cardapio desejado.
+    :return: objeto da classe Cardapio que contem o cardapio requisitado.
+    """
     res = requests.get(URL_TEMPLATE+data_string) # faz o request para a pagina da prefeitura
 
     html_doc = res.content # pega a pagina HTML
     soup = BeautifulSoup(html_doc, 'html.parser')
     meals = soup.find_all(class_="fundo_cardapio") # pega todos os elementos da classe fundo_cardapio. Dos 5 encontrados, 4 sao os cardapios das 4 refeicoes.
 
-    tipos_refeicoes = list(TipoRefeicao) # lista os ti
+    tipos_refeicoes = list(TipoRefeicao) # lista os tipos de refeicao para iterarmos sobre eles.
 
     refeicoes = {}
 
@@ -89,16 +91,20 @@ def cardapio_por_data(data_string):
         refeicoes[tipos_refeicoes[i]] = get_refeicao(tipos_refeicoes[i].value, m)
 
 
-    cardapio = Cardapio.fromRefeicoesDict(data=data_string, refeicoes=refeicoes)
-
-    return cardapio
+    return Cardapio.fromRefeicoesDict(data=data_string, refeicoes=refeicoes) # instacia um objeto Cardapio
 
 
 
 
 
-# retorna um dict com o cardapio completo do dia para cada data.
+
 def cardapio_para_datas(data_strings):
+    """
+    Dada uma lista com strings das datas, essa funcao retorna os objetos Cardapio desses cardapios em uma lista.
+
+    :param data_strings: lista com strings das datas dos cardapios desejados.
+    :return: lista com objetos da classe Cardapio que contem os cardapios das datas fornecidas.
+    """
     cardapios = []
 
     for data in data_strings:
@@ -110,33 +116,19 @@ def cardapio_para_datas(data_strings):
 
 @cache.memoize(timeout=60 * 5) # cache com timeout de 5min
 def get_next_cardapios(date_string, next):
-    
+    """
+    Fornece os cardapios dos *next* dias a partir da data fornecida em *date_string*.
+
+    Ponto de entrada principal. A view que sera chamada no app sera essa.
+
+
+    :param date_string: data inicial.
+    :param next: inteiro que representa a quantidade de cardapios desejados a partir da data inicial.
+    :return: lista com os objetos Cardapio contendo os cardapios das datas requisitadas.
+    """
+
     date_strings = date_services.next_weekdays(next, start_date=date_string)
     print("EXECUTOU get_next_cardapio")
     return cardapio_para_datas(date_strings)
 
 
-
-if __name__ == '__main__':
-    main()
-
-
-def cardapio_por_data_offline():
-    f = open("cardapio_offline.html")
-
-    html_doc = f.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    meals = soup.find_all(class_="fundo_cardapio")
-
-    tipos_refeicoes = list(TipoRefeicao)
-
-    refeicoes = {}
-
-    for i, m in enumerate(meals[1:]):
-        refeicoes[tipos_refeicoes[i]] = get_refeicao(tipos_refeicoes[i].value, m)
-
-
-    cardapio = Cardapio.fromRefeicoesDict(data='2017-06-09', refeicoes=refeicoes)
-    print("cardapio = ", cardapio)
-
-    return cardapio
