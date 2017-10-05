343a2d8019a47a89f40850136a04069c3f8b9a87
Gustavo Avena <ggavena@gmail.com>
2017-10-05 04:42:36 -0300

    able to send notifications to a batch of tokens

--- cardapio_notifications.py
+++ cardapio_notifications.py
@@ -80,32 +80,22 @@ def delete_token(token):
 
 
 
+
 def push_next_notification():
 
-    apns = APNs(use_sandbox=True, cert_file='./../Certificates/bandex_push_notifications_dev_cert.pem',
-                key_file='./../Certificates/bandex_push_notifications_dev_key.pem')
+    db = setup_firebase()
+    tokens = db.child('tokens').get().val()
 
-    frame = Frame()
-    identifier = 1
-    expiry = time.time() + 3600
-    priority = 10
+
+
+    # token_hex = 'e77c39f01e46911ae21bc93a57dc55ca29d9a81325a22cc4fee340c75a2957d9'
+
     payload = Payload(alert="Hello World!", sound="default", badge=1)
     # TODO: pegar refeicao apropriada.
 
 
-    db = setup_firebase()
-    tokens = db.child('tokens')
+    topic = 'com.Gustavo.Avena.BandecoUnicamp'
+    client = APNsClient('./../Certificates/bandex_push_notifications_dev_key.pem', use_sandbox=True, use_alternative_port=False)
 
-
-
-    token_hex = 'b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b87'
-    payload = Payload(alert="Hello World!", sound="default", badge=1)
-    client = APNsClient('key.pem', use_sandbox=True, use_alternative_port=False)
-    client.send_notification(token_hex, payload, topic)
-
-    for t in tokens:
-            print(t)
-            frame.add_item(t, payload, identifier, expiry, priority)
-
-    apns.gateway_server.send_notification_multiple(frame)
+    client.send_notification_batch(tokens, payload, topic)
 
